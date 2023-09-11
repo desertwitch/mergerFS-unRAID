c7d2b6123e7eafc59730968144bf4f393d1c6813
desertwitch <24509509+desertwitch@users.noreply.github.com>
2023-09-11 20:59:01 +0200

    changes

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -6,9 +6,18 @@ DOCROOT="/usr/local/emhttp/plugins/mergerfsp"
 chmod +0755 $DOCROOT/event/*
 chmod +0755 $DOCROOT/mergerfs/*
 
+if [ ! -d /etc/mergerfsp ]; then
+    mkdir /etc/mergerfsp
+fi
+
 if [ ! -d $BOOT/scripts ]; then
     mkdir $BOOT/scripts
 fi
 
 cp -nr $DOCROOT/mergerfs/* $BOOT/scripts >/dev/null 2>&1
-chmod +0755 $BOOT/scripts/*
+cp -rf $BOOT/scripts/* /etc/mergerfsp >/dev/null 2>&1
+
+chmod 755 /etc/mergerfsp
+chown root:root /etc/mergerfsp
+chmod 755 /etc/mergerfsp/*
+chown root:root /etc/mergerfsp/*

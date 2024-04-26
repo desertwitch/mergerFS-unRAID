a0aa1e815e4073445780d9fc4d8da23fd0490b9c
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-04-26 16:36:17 +0200

    changes

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+# Copyright Derek Macias (parts of code from NUT package)
+# Copyright macester (parts of code from NUT package)
+# Copyright gfjardim (parts of code from NUT package)
+# Copyright SimonF (parts of code from NUT package)
+# Copyright desertwitch
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License 2
+# as published by the Free Software Foundation.
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
 BOOT="/boot/config/plugins/mergerfsp"
 DOCROOT="/usr/local/emhttp/plugins/mergerfsp"
 
--- source/usr/local/emhttp/plugins/mergerfsp/include/mergerfsp_config.php
+++ source/usr/local/emhttp/plugins/mergerfsp/include/mergerfsp_config.php
@@ -1,5 +1,22 @@
 <?
-
+/* Copyright Derek Macias (parts of code from NUT package)
+ * Copyright macester (parts of code from NUT package)
+ * Copyright gfjardim (parts of code from NUT package)
+ * Copyright SimonF (parts of code from NUT package)
+ * Copyright desertwitch
+ *
+ * Copyright Dan Landon
+ * Copyright Bergware International
+ * Copyright Lime Technology
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ */
 $mergerfsp_cfg  = parse_ini_file("/boot/config/plugins/mergerfsp/mergerfsp.cfg");
 
 $mergerfsp_timer_mounts = trim(isset($mergerfsp_cfg['TIMERMOUNTS']) ? htmlspecialchars($mergerfsp_cfg['TIMERMOUNTS']) : '20');

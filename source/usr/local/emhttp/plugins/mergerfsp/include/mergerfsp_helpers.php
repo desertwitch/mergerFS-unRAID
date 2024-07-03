2c09b69c535871a1643f0dd89b3f286f492589a9
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-07-03 10:37:22 +0200

    changes

---
+++ source/usr/local/emhttp/plugins/mergerfsp/include/mergerfsp_helpers.php
@@ -0,0 +1,42 @@
+<?
+/* Copyright Derek Macias (parts of code from NUT package)
+ * Copyright macester (parts of code from NUT package)
+ * Copyright gfjardim (parts of code from NUT package)
+ * Copyright SimonF (parts of code from NUT package)
+ * Copyright desertwitch
+ *
+ * Copyright Dan Landon
+ * Copyright Bergware International
+ * Copyright Lime Technology
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ */
+function msfp_humanFileSize($sizeObj,$unit="") {
+    try {
+        $size = intval($sizeObj);
+        if($size) {
+            if( (!$unit && $size >= 1000000000000) || $unit == "TB")
+                return rtrim(rtrim(number_format(($size/1000000000000),2), "0"), ".") . " TB";
+            if( (!$unit && $size >= 1000000000) || $unit == "GB")
+                return rtrim(rtrim(number_format(($size/1000000000),2), "0"), ".") . " GB";
+            if( (!$unit && $size >= 1000000) || $unit == "MB")
+                return rtrim(rtrim(number_format(($size/1000000),2), "0"), ".") . " MB";
+            if( (!$unit && $size >= 1000) || $unit == "KB")
+                return rtrim(rtrim(number_format(($size/1000),2), "0"), ".") . " KB";
+            return number_format($size) . " B";
+        } else {
+            return "-";
+        }
+    } catch (Throwable $e) { // For PHP 7
+        return "-";
+    } catch (Exception $e) { // For PHP 5
+        return "-";
+    }
+}
+?>
